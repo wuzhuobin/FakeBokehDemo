6b01fabbd128070536e1489624df829542c82929
gundam <gundam@lenovo>
2019-02-21 00:37:52 +0800

    with result

--- grabcut.py
+++ grabcut.py
@@ -1,8 +1,4 @@
 #%%
-# <a href="https://colab.research.google.com/github/googlecolab/colabtools/blob/master/notebooks/colab-github-demo.ipynb">
-#  <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
-# </a>
-#%%
 import cv2;
 import matplotlib.pyplot;
 import numpy;
